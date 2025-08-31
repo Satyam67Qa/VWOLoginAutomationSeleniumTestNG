c003d3d96411126f231e694b92ed0505fee268d9
Satyam67Qa <satyamsingh28389@gmail.com>
2025-08-31 18:01:47 +0530

    maven

---
+++ src/test/java/com/Mavan/TestCasess2.java
@@ -0,0 +1,28 @@
+package com.Mavan;
+
+import org.testng.annotations.Test;
+
+public class TestCasess2 {
+
+    @Test
+    public void a1(){
+        System.out.println("A1");
+    }
+    @Test
+    public void a2(){
+        System.out.println("A2");
+    }
+    @Test
+    public void a3(){
+        System.out.println("A3");
+    }
+    @Test
+    public void a4(){
+        System.out.println("A4");
+    }
+    @Test
+    public void a5(){
+        System.out.println("A5");
+    }
+
+}
